11f26e43fedcdf50a4d0a846a2ee79d1f5540253
bendaxia <916917649@qq.com>
2017-11-27 15:31:29 +0800

    js html 优化

--- apicenter_html/js/api/apiDetails.js
+++ apicenter_html/js/api/apiDetails.js
@@ -1,3 +1,48 @@
+function listWithGroup() {
+	$.ajax({
+				url : WebApplicationPath + "/api/group/list/catalog",
+				type : "post",
+				dataType : "json",
+				success : function(result) {
+					var interface_html = "";
+					var option_html = "";
+					if (result.code != 200) {
+						alert(result.message);
+						$("#interfaceType_id").html(option_html);
+						$("#interfaceTypeId").html(interface_html);
+						return;
+					}
+					for (var i = 0; i < result.data.length; i++) {
+						var infa = result.data[i];
+						interface_html += ('<ul  class="nav nav-pills nav-stacked">'+ '<li role="presentation" class="group"><a href="javascript:void(0)"><i class="glyphicon glyphicon-equalizer"></i>'+ infa.gruopName + '</a>' + '<ul  class="nav nav-pills nav-stacked" >');
+						option_html += ('<option value="0">'+ infa.gruopName + '</option>');
+						for (var y = 0; y < infa.apiGroupDetails.length; y++) {
+							var ins = infa.apiGroupDetails[y];
+							interface_html += ('<li role="presentation">'+ '<a href="/apicenter_html/html/api/apiDetails.html?apiId='+ ins.apiId + '">' + ins.apiName + '</a>' + '</li>');
+						}
+						interface_html += ('</ul>' + '</li>' + '</ul>');
+					}
+					$("#interfaceTypeId").html(interface_html);
+					initLeft();// 菜单效果
+				}
+			});
+}
+//菜单效果
+function initLeft(){
+	$(".nav-pills>li.group>a").click(function(){
+		var pt = $(this).parent().toggleClass("click");
+		if(pt.hasClass("click")){
+			pt.children("ul").children().slideUp("fast");
+		}else{
+			pt.children("ul").children().slideDown("fast");
+		}
+		return false;
+	});
+}
+/**
+ * 获取api
+ * @returns
+ */
 function getApi(){
 	var apiId = getUrlParam('apiId');
 	$.ajax({
@@ -68,4 +113,13 @@ function getApi(){
 			$("#pid").html(param_html);
 		}
 	});
+}
+
+/**
+ * 修改
+ * @returns
+ */
+function update(){
+	var id = getUrlParam('apiId');
+	window.location = "apiUpdate.html?id=" + id;
 }
