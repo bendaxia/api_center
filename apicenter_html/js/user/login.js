75f113aba9f2a3751d886b31a693e449baa7ebbf
bendaxia <916917649@qq.com>
2017-12-01 10:35:52 +0800

    优化登陆体验

--- apicenter_html/js/user/login.js
+++ apicenter_html/js/user/login.js
@@ -1,32 +1,40 @@
-function login(){
+document.onkeydown = function(event) {
+	var e = event || window.event || arguments.callee.caller.arguments[0];
+	if (e && e.keyCode == 13) { // enter 键
+		login();
+	}
+};
+function login() {
 	var userNumber = $("#userNumber").val();
 	var passWord = $("#passWord").val();
-	if(userNumber==""){
+	if (userNumber == "") {
 		alert("请填写账号");
 		return;
 	}
-	if(passWord==""){
+	if (passWord == "") {
 		alert("请填写密码");
 		return;
 	}
-	 //ajax请求
- 	$.ajax({
-		type : "post",
-		url : WebApplicationPath+"/login/login",
-		contentType: "application/x-www-form-urlencoded;charset=utf-8",
-		dataType : "json",
-		data : {
-			userNumber:userNumber,
-			passWord:passWord
-		},
-		success : function(data) {
-			alert(data.message);
-			if(data.code==200){
-				window.location.href="/apicenter_html/html/api/apiIndex.html"; 
-			}
-		},
-		error : function(XMLHttpRequest, textStatus, errorThrown) {
-			alert("请求失败:这真是个悲伤的故事。");
-		}
-	});   
+	// ajax请求
+	$
+			.ajax({
+				type : "post",
+				url : WebApplicationPath + "/login/login",
+				contentType : "application/x-www-form-urlencoded;charset=utf-8",
+				dataType : "json",
+				data : {
+					userNumber : userNumber,
+					passWord : passWord
+				},
+				success : function(data) {
+					if (data.code != Response_Code_OK) {
+						alert(data.message);
+						return;
+					}
+					window.location.href = "/apicenter_html/html/api/apiIndex.html";
+				},
+				error : function(XMLHttpRequest, textStatus, errorThrown) {
+					alert("请求失败:这真是个悲伤的故事。");
+				}
+			});
 }
